d87de90de6f5698543d3edb0f1c42220fc830731
Wendel25 <luccawendel25@gmail.com>
2025-02-11 23:36:28 -0300

    create tables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from 'src/config/consts/consts';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: DB_HOST,
+      port: 3306,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      entities: [__dirname + '/../**/*.entity.{ts,js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api/music_secretary');
+
+  app.enableCors({
+    origin: ['http://localhost:5000'],
+    methods: 'GET,PUT,POST,DELETE',
+    credentials: true,
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Music Secretary')
+    .setDescription(`This project was created with NEST version: <b>11.0.2</b>.`)
+    .setVersion('1.0')
+    // .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
