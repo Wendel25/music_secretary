d47ba5e86cde5b51aed9a6ab7567b5c8e26fba96
Wendel25 <luccawendel25@gmail.com>
2025-03-08 00:50:31 -0300

    news implements

---
+++ frontend/src/hook/use-device-type/index.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+type DeviceType = "mobile" | "tablet" | "desktop";
+
+const getDeviceType = (width: number): DeviceType => {
+    if (width < 768) return "mobile";
+    if (width >= 768 && width < 1024) return "tablet";
+    return "desktop";
+};
+
+const useDeviceType = (): DeviceType => {
+    const [deviceType, setDeviceType] = useState<DeviceType>(
+        getDeviceType(window.innerWidth)
+    );
+
+    useEffect(() => {
+        const handleResize = () => setDeviceType(getDeviceType(window.innerWidth));
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return deviceType;
+};
+
+export default useDeviceType;
--- frontend/src/view/unauthenticated/login/index.tsx
+++ frontend/src/view/unauthenticated/login/index.tsx
@@ -1,16 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export function ViewLogin() {
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <Button variant={"destructive"} type="submit">
-          Login
-        </Button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <Card className="w-full max-w-4xl grid md:grid-cols-2 overflow-hidden mt-[40%] sm:mt-20">
+        <div className="p-6 flex flex-col max-h-[60vh] sm:max-h-[70vh] overflow-auto">
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">Entrar</CardTitle>
+            <p className="text-muted-foreground">Entre com suas credenciais para acessar sua conta</p>
+          </CardHeader>
+          <CardContent className="flex flex-col flex-grow">
+            <p>teste</p>
+          </CardContent>
+        </div>
+      </Card>
     </div>
   );
 }
